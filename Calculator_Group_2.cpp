989c99bb4244805ffcde468fcbd58b90019aec2f
INFALOUS <33593931+INFALOUS@users.noreply.github.com>
2020-11-30 12:19:51 +0600

    Add files via upload

--- Calculator_Group_2.cpp
+++ Calculator_Group_2.cpp
@@ -6,16 +6,19 @@
 
 using namespace std;
 
-string     Vvod;
+string Vvod;
 vector<int> PervoeChislo;
 vector<int> VtoroeChislo;
 vector<int> Resultat;
 
-void Fill();
+void Zapolnenie();
+void Umnozhenie();
 void Summa();
+void Vichitanie();
 void PokazRezultata();
-int Perevod(char simbol);
-int checkString();
+int Perevod(char Simvol);
+int ProverkaStroki();
+int ProverkaNaBolshe();
 
 int main() {
 	setlocale(LC_ALL, "Russian");
@@ -23,23 +26,40 @@ int main() {
 	do {
 		cout << "Введите первое число, операнд и второе число (1+1)" << endl;
 		getline(cin, Vvod);
-		if (checkString()) {
+		if (ProverkaStroki()) {
 			system("cls");
 			cout << "Выражение некорректно, введите верное" << endl;
 			system("pause");
 		}
 		system("cls");
-	} while (checkString());
+	} while (ProverkaStroki());
 
-	Fill();
-	Summa();
+	Zapolnenie();
+	ProverkaNaBolshe();
+	//Summa();
+	Vichitanie();
+	Umnozhenie();
 	PokazRezultata();
 
 	system("pause");
 	return 0;
 }
 
-int checkString() {
+int ProverkaNaBolshe() {
+	if (PervoeChislo.size() > VtoroeChislo.size())
+		return 1;
+	else if (VtoroeChislo.size() > PervoeChislo.size())
+		return 0;
+
+	for (int i = 0; i < PervoeChislo.size(); i++) {
+		if (PervoeChislo[i] > VtoroeChislo[i])
+			return 1;
+		else if (VtoroeChislo[i] > PervoeChislo[i])
+			return 0;
+	}
+}
+
+int ProverkaStroki() {
 	int counter = 0;
 
 	for (int i = 0; i < Vvod.length(); i++) {
@@ -54,7 +74,7 @@ int checkString() {
 		return false;
 }
 
-void Fill() {
+void Zapolnenie() {
 	int flag = 0;
 
 	for (int i = 0; i < Vvod.length(); i++) {
@@ -128,3 +148,83 @@ void PokazRezultata() {
 		cout << Resultat[i];
 }
 
+void Vichitanie() {
+	int flag = 0;
+	auto begin = Resultat.cbegin();
+
+	if (PervoeChislo.size() > VtoroeChislo.size()) {
+		for (int i = PervoeChislo.size() - VtoroeChislo.size(); i > 0; i--) {
+			VtoroeChislo.insert(VtoroeChislo.begin(), 0);
+		}
+	}
+	else if (VtoroeChislo.size() > PervoeChislo.size()) {
+		for (int i = VtoroeChislo.size() - PervoeChislo.size(); i > 0; i--) {
+			PervoeChislo.insert(PervoeChislo.begin(), 0);
+		}
+	}
+
+	if (ProverkaNaBolshe()) {
+		for (int i = PervoeChislo.size() - 1; i >= 0; i--) {
+			if (PervoeChislo[i] < VtoroeChislo[i]) {
+				Resultat.insert(Resultat.begin(), (PervoeChislo[i] + 10) - VtoroeChislo[i]);
+				int Schet = i;
+				while (Schet > 0) {
+					if (PervoeChislo[Schet - 1] > 0) {
+						PervoeChislo[Schet - 1] = PervoeChislo[Schet - 1] - 1;
+						flag++;
+						if (flag == 2)
+							PervoeChislo[Schet - 1] += 1;
+						break;
+					}
+					else {
+						PervoeChislo[Schet - 1] = -1;
+						Schet--;
+					}
+				}
+			}
+			else {
+				Resultat.insert(Resultat.begin(), PervoeChislo[i] - VtoroeChislo[i]);
+			}
+			if (PervoeChislo[i] < 0 && i == 0) {
+				Resultat[i] = 0;
+			}
+		}
+		for (int i = 0; i < Resultat.size(); i++) {
+			if (Resultat[i] < 0) {
+				Resultat[i] = 0;
+			}
+		}
+	}
+	else {
+		for (int i = PervoeChislo.size() - 1; i >= 0; i--) {
+			if (VtoroeChislo[i] < PervoeChislo[i]) {
+				Resultat.insert(Resultat.begin(), (VtoroeChislo[i] + 10) - PervoeChislo[i]);
+				int Schet = i;
+				while (Schet > 0) {
+					if (VtoroeChislo[Schet - 1] != 0) {
+						VtoroeChislo[Schet - 1] = VtoroeChislo[Schet - 1] - 1;
+						flag++;
+						if (flag == 2)
+							VtoroeChislo[Schet - 1] += 1;
+						break;
+					}
+					else {
+						VtoroeChislo[Schet - 1] = -1;
+						Schet--;
+					}
+				}
+			}
+			else {
+				Resultat.insert(Resultat.begin(), VtoroeChislo[i] - PervoeChislo[i]);
+			}
+			if (VtoroeChislo[i] < 0 && i == 0) {
+				Resultat[i] = 0;
+			}
+		}
+		cout << "-";
+	}
+}
+
+void Umnozhenie() {
+
+}
